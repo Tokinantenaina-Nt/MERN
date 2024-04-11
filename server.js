d77b8201378ddcd96c9aa16cd9e62284a08fff2c
Tokinantenaina <tokib2oba@yahoo.com>
2024-04-11 05:31:27 +0300

    parti back-end

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 const upload_checking_error = require("./middleware/uploadError.middleware");
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000" /* ajouter l'URL à autoriser ici */
+];
 const corsOptions = {
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
